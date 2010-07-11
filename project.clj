88be850571d828e09177f2c8d303282e2e58e2e7
Alex Feinberg <alex@strlen.net>
2010-07-11 11:35:41 -0700

    Populated project.clj with deps, updated README.

--- project.clj
+++ project.clj
@@ -1,5 +1,12 @@
 (defproject clomert "0.0.1"
   :description "Voldemort Clojure Client/Examples"
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [com.google.collections/google-collections "1.0"]
+                 [commons-lang/commons-lang "2.4"]
+                 [jdom/jdom "1.1"]
+                 [log4j/log4j "1.2.14"]
+                 [commons-pool/commons-pool "1.5.2"]
+                 [voldemort/voldemort "0.81"]
+                 [voldemort/voldemort-contrib "0.81"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
