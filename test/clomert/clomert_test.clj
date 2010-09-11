cff21e2b135bbe8322b8e7ea9af641c96c0ec678
Alex Feinberg <alex@strlen.net>
2010-09-11 00:49:48 -0700

    Bump up version, increase test coverage

--- test/clomert/clomert_test.clj
+++ test/clomert/clomert_test.clj
@@ -15,4 +15,27 @@
                   (:put "hello" "world")
                   (:get-value "hello"))))))
 
-  
+(deftest store-get-all
+  (let [client (v/make-store-client *factory* "test")
+        res (v/do-store client
+                        (:put "russia" "moscow")
+                        (:put "belarus" "minsk")
+                        (:put "ukraine" "kiev")
+                        (:get-all ["russia" "belarus" "ukraine"]))]
+    (is
+     (and (= (v/versioned-value (get res "russia")) "moscow")
+          (= (v/versioned-value (get res "belarus")) "minsk")
+          (= (v/versioned-value (get res "ukraine")) "kiev")))))
+
+(deftest store-apply-update
+  (let [client (v/make-store-client *factory* "test")]
+    (v/store-apply-update
+     5 client
+     (fn [#^StoreClient scl]
+       (let [v (v/store-get scl "foo")]
+         (v/store-conditional-put scl "foo"
+                                  (if (nil? v)
+                                    (v/make-versioned "a")
+                                    (v/versioned-set-value!
+                                     (v (str (v/versioned-value v)
+                                             "b"))))))))))
